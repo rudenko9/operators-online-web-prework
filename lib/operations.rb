af2101a6e01a0e0bdbe19d77c95cd2b96502c887
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-22 03:11:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,3 @@
-def unsafe?(speed)
-  speed = 79
-  if speed > 60
- true
- speed = 35
- elsif speed < 40
- true
- speed = 50
- else speed = 50 
- true
-     end
-     end
 def not_safe?(speed)
   speed = 79 and 79 > 60? true : false
   speed = 35 and 35 < 40? true : false
